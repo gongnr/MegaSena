d130c659e463d64f4f125b683210e598f73e7913
gongnr <37491416+gongnr@users.noreply.github.com>
2021-04-21 17:40:34 -0300

    Update Gerador de sena.py

--- Gerador de sena.py
+++ Gerador de sena.py
@@ -2,12 +2,11 @@
 """
 Spyder Editor
 
-This is a temporary script file.
 """
 import random
 sena = []
 while len(sena) != 6:
-    x = random.randint(1, 61)
+    x = random.randint(1, 60)
     if x not in sena:
         sena.append(x)
 print(sena)
