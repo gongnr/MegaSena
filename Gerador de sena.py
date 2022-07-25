67a7c63977e1f4400c944f3b6a896caa21fcdffc
Sergio_Gontijo <37491416+gongnr@users.noreply.github.com>
2022-07-24 21:24:06 -0300

    Update Gerador de sena.py
    Add comment and final message.

--- Gerador de sena.py
+++ Gerador de sena.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-"""
+#Script that generate a list of 6 different numbers
 import random
 sena = []
 while len(sena) != 6:
     x = random.randint(1, 60)
     if x not in sena:
         sena.append(x)
-sena.sort()        
+sena.sort() 
+print ("$Boa Sorte!$")       
 print(sena)
